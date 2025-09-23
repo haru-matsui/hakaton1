c3d0b151c34fc00f9935ad78b5b4f78217c35dd8
damir <106244014+damka22@users.noreply.github.com>
2025-09-23 14:27:09 +0500

    Add files via upload
    load files

---
+++ hacaton/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+# Простые данные
+auditoriums = [
+    {"number": "5-101", "building": "Главный корпус", "floor": "1 этаж", "description": "Большая лекционная аудитория"},
+    {"number": "205", "building": "Главный корпус", "floor": "2 этаж", "description": "Компьютерный класс"},
+    {"number": "301", "building": "Техническое здание", "floor": "3 этаж", "description": "Лаборатория физики"},
+    {"number": "15", "building": "Библиотека", "floor": "1 этаж", "description": "Читальный зал"},
+    {"number": "110", "building": "Главный корпус", "floor": "1 этаж", "description": "Актовый зал"},
+]
+
+events = [
+    {"name": "День открытых дверей", "date": "25 декабря", "place": "Актовый зал"},
+    {"name": "Хакатон для студентов", "date": "30 декабря", "place": "Компьютерный класс"},
+    {"name": "Новогодний концерт", "date": "28 декабря", "place": "Актовый зал"},
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    query = request.args.get('q', '')
+    results = []
+    
+    if query:
+        for room in auditoriums:
+            if query.lower() in room['number'].lower() or query.lower() in room['description'].lower():
+                results.append(room)
+    
+    return render_template('search.html', results=results, query=query)
+
+@app.route('/events')
+def events_page():
+    return render_template('events.html', events=events)
+
+@app.route('/clubs')
+def clubs():
+    # Загружаем данные о клубах из JSON файла
+    try:
+        with open('static/data/clubs.json', 'r', encoding='utf-8') as f:
+            clubs_data = json.load(f)
+        return render_template('clubs.html', clubs=clubs_data['clubs'])
+    except FileNotFoundError:
+        # Если файл не найден, возвращаем пустой список
+        return render_template('clubs.html', clubs=[])
+
+@app.route('/faq')
+def faq_page():
+    return render_template('faq.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
